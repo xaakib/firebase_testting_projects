49d1cbfab4ee64cbd5cabbe17902fd9ca91f5208
Xaakib <64243974+xaakib@users.noreply.github.com>
2020-09-24 20:54:26 +0600

    changed

--- lib/singin_firebase.dart
+++ lib/singin_firebase.dart
@@ -1,3 +1,6 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_testting/firebase_listtile.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class FirebaseInputeData extends StatefulWidget {
@@ -6,14 +9,81 @@ class FirebaseInputeData extends StatefulWidget {
 }
 
 class _FirebaseInputeDataState extends State<FirebaseInputeData> {
+  var email;
+  var password;
+
+  register() async {
+    FirebaseAuth firebaseAuth = FirebaseAuth.instance;
+    final FirebaseUser user = (await firebaseAuth
+            .createUserWithEmailAndPassword(email: email, password: email))
+        .user;
+
+    if (user != null) {
+      Navigator.push(
+          context, CupertinoPageRoute(builder: (context) => DetailsScreen()));
+    } else {
+      print("Error");
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        height: 200,
-        color: Colors.red,
-        child: Text('data'),
-      ),
+    return SafeArea(
+      child: Scaffold(
+          body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "registation",
+                style: TextStyle(
+                  fontSize: 35,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              TextFormField(
+                onChanged: (value) {
+                  setState(() {
+                    email = value;
+                  });
+                },
+              ),
+              TextFormField(
+                onChanged: (value) {
+                  setState(() {
+                    password = value;
+                  });
+                },
+              ),
+              SizedBox(
+                height: 50,
+              ),
+              RaisedButton(
+                  child: Text(
+                    "SingUp",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  onPressed: null),
+              Text(
+                "Already Regiostared?",
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              RaisedButton(onPressed: register),
+            ],
+          ),
+        ),
+      )),
     );
   }
 }
